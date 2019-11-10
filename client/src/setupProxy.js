53d1f3c4d61a64f1d3ba9087b85e0f222f7236a4
Jan Mathew <janmathew@MacBookPro.local>
2019-11-10 10:37:13 +0800

    added proxy middleware to resolve issues with CRA 3.0

---
+++ client/src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = app => {
+	app.use(
+		proxy(['/api', '/auth/google', '/auth/facebook'], {
+			target: 'http://localhost:5000'
+		})
+	);
+};

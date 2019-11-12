f5a96797bf695afa9846ef582e0d429743642a94
Jan Mathew <janmathew@MacBookPro.local>
2019-11-13 00:24:50 +0800

    added react-redux, redux boilerplate and react-router

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+const renderApp = () => {
+	return (
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
+};
+
+render(renderApp(), rootElement);
 
 // If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// unregister() to register() below. Note: this comes with some pitfalls.
 serviceWorker.unregister();

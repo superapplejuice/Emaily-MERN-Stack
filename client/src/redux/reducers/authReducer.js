027200f4dfa242eef7c63142618e08bfd07d9690
Jan Mathew <janmathew@MacBookPro.local>
2019-11-13 22:43:21 +0800

    boilerplate action types, refactored to use types in authReducer

--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,13 +1,15 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const initialState = {
   isSignedIn: false
 };
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true, userId: payload };
 
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false, userId: null };
 
     default:

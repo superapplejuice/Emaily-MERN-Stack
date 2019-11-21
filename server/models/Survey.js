1c72a65b0102b1efe7aa1b030259b83b141c533d
Jan Mathew <janmathew@MacBookPro.local>
2019-11-21 21:12:32 +0800

    updated Survey schema with relational field and date fields

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -11,7 +11,15 @@ const surveySchema = new Schema({
   // recipients now contains an array of RecipientSchema records
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 }
+  no: { type: Number, default: 0 },
+  // establishing relationship with the User schema
+  // every survey belongs to a particular user
+  // assigns the Id of the User that owns a survey record here
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  // record date survey was sent to user
+  dateSent: Date,
+  // record when user has responded to the survey
+  lastResponded: Date
 });
 
 mongoose.model("surveys", surveySchema);

a73ae88bade713fb52447b5fdde325097e150c1c
Jan Mathew <janmathew@MacBookPro.local>
2019-11-23 15:23:52 +0800

    defined Mailer constructor

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,11 +1,22 @@
 const sendgrid = require("sendgrid");
-const { Mail } = sendgrid.mail;
+const { Mail, Email, Content } = sendgrid.mail;
 const { sendGridKey } = require("../config/keys");
 
 // the Mailer class is an extension of helper.Mail
 // i.e. it has access to all functionality from helper.Mail
 class Mailer extends Mail {
-  constructor() {}
+  constructor(header, content) {
+    // executes the constructor on every instance of the Mailer class
+    super();
+
+    const { subject, recipients } = header;
+
+    // required by SendGrid
+    this.from_email = new Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
 }
 
 module.exports = Mailer;

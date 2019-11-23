f59a637e688c0a01effbf265b77626b4049297b8
Jan Mathew <janmathew@MacBookPro.local>
2019-11-23 15:42:06 +0800

    more Mailer class config

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,5 +1,5 @@
 const sendgrid = require("sendgrid");
-const { Mail, Email, Content } = sendgrid.mail;
+const { Mail, Email, Content, TrackingSettings, ClickTracking } = sendgrid.mail;
 const { sendGridKey } = require("../config/keys");
 
 // the Mailer class is an extension of helper.Mail
@@ -11,11 +11,34 @@ class Mailer extends Mail {
 
     const { subject, recipients } = header;
 
-    // required by SendGrid
+    // getting required info to send the email
     this.from_email = new Email("no-reply@emaily.com");
     this.subject = subject;
     this.body = new Content("text/html", content);
     this.recipients = this.formatAddresses(recipients);
+
+    // execute methods
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  // return the email addresses in the recipeints array
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+      return new Email(email);
+    });
+  }
+
+  // track the users who clicked the links in the email
+  // required by SendGrid for click tracking to work
+  addClickTracking() {
+    const trackingSettings = new TrackingSettings();
+    const clickTracking = new ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+
+    this.addTrackingSettings(trackingSettings);
   }
 }
 

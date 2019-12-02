36d1cd2cf500024031a2c159273ecb7fcf1024e5
Jan Mathew <janmathew@MacBookPro.local>
2019-12-02 19:49:41 +0800

    refactored SurveyForm to take props instead of hardcoded values

--- client/src/components/form/SurveyForm.jsx
+++ client/src/components/form/SurveyForm.jsx
@@ -1,6 +1,6 @@
-import React, { Fragment } from "react";
-import { Formik, Form } from "formik";
-import * as yup from "yup";
+import React, { Fragment } from 'react'
+import { Formik, Form } from 'formik'
+import * as yup from 'yup'
 
 import {
   surveyTitle,
@@ -11,58 +11,45 @@ import {
   subject_line,
   email_body,
   recipient_list
-} from "./form-utils/types";
-import FormField from "./form-utils/FormField";
+} from './form-utils/types'
+import FormField from './form-utils/FormField'
 
-const SurveyForm = ({ header, formAction }) => {
-  const initialValues = {
-    surveyTitle: "",
-    subjectLine: "",
-    emailBody: "",
-    recipientList: ""
-  };
+const SurveyForm = ({
+  initialValues,
+  validationSchema,
+  header,
+  formAction
+}) => (
+  <Fragment>
+    <h4>{header}</h4>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema(yup)}
+      onSubmit={(values, { setSubmitting }) => {
+        formAction(values)
+        setSubmitting(false)
+      }}
+    >
+      {({ isSubmitting }) => {
+        return (
+          <Form>
+            <FormField name={surveyTitle} label={survey_title} />
+            <FormField name={subjectLine} label={subject_line} />
+            <FormField name={emailBody} label={email_body} />
+            <FormField name={recipientList} label={recipient_list} />
+            <button
+              className='btn waves-effect waves-light'
+              type='submit'
+              disabled={isSubmitting}
+            >
+              Submit
+              <i className='material-icons right'>send</i>
+            </button>
+          </Form>
+        )
+      }}
+    </Formik>
+  </Fragment>
+)
 
-  const validationSchema = yup.object().shape({
-    surveyTitle: yup.string().required("Please enter a title"),
-    subjectLine: yup.string().required("Please enter a subject line"),
-    emailBody: yup.string().required("Please enter the body of the email"),
-    recipientList: yup
-      .string()
-      .required("Please enter email addresses to send your survey to")
-  });
-
-  return (
-    <Fragment>
-      <h4>{header}</h4>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={(values, { setSubmitting }) => {
-          formAction(values);
-          setSubmitting(false);
-        }}
-      >
-        {({ isSubmitting }) => {
-          return (
-            <Form>
-              <FormField name={surveyTitle} label={survey_title} />
-              <FormField name={subjectLine} label={subject_line} />
-              <FormField name={emailBody} label={email_body} />
-              <FormField name={recipientList} label={recipient_list} />
-              <button
-                className="btn waves-effect waves-light"
-                type="submit"
-                disabled={isSubmitting}
-              >
-                Submit
-                <i className="material-icons right">send</i>
-              </button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </Fragment>
-  );
-};
-
-export default SurveyForm;
+export default SurveyForm
--- client/src/components/survey/SurveyNew.jsx
+++ client/src/components/survey/SurveyNew.jsx
@@ -1,14 +1,38 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react'
+import { connect } from 'react-redux'
 
-import { postSurveyAction } from "../../redux/actions";
+import { postSurveyAction } from '../../redux/actions'
 
-import SurveyForm from "../form/SurveyForm";
+import SurveyForm from '../form/SurveyForm'
 
 const SurveyNew = ({ postSurveyAction }) => {
-  const header = "Create a new Survey";
+  const initialValues = {
+    surveyTitle: '',
+    subjectLine: '',
+    emailBody: '',
+    recipientList: ''
+  }
 
-  return <SurveyForm header={header} formAction={postSurveyAction} />;
-};
+  const validationSchema = yup =>
+    yup.object().shape({
+      surveyTitle: yup.string().required('Please enter a title'),
+      subjectLine: yup.string().required('Please enter a subject line'),
+      emailBody: yup.string().required('Please enter the body of the email'),
+      recipientList: yup
+        .string()
+        .required('Please enter email addresses to send your survey to')
+    })
 
-export default connect(undefined, { postSurveyAction })(SurveyNew);
+  const header = 'Create a new Survey'
+
+  return (
+    <SurveyForm
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      header={header}
+      formAction={postSurveyAction}
+    />
+  )
+}
+
+export default connect(undefined, { postSurveyAction })(SurveyNew)
